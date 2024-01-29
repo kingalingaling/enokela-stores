7440092cc9c8754f1dfed99bf703ac3a0aa46397
King <kingeshiebor@gmail.com>
2024-01-29 19:05:01 +0100

    Fastforward commit

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,7 @@
+function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex items-center h-full justify-center">{children}</div>
+  );
+}
+
+export default AuthLayout;
---
+++ components/navbar.tsx
@@ -0,0 +1,22 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+import { MainNav } from "./mainNav";
+
+const Navbar = () => {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <div>
+            Store Switcher
+        </div>
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+            <UserButton afterSignOutUrl="/"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
