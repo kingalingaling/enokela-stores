e6f646eaa77c8d3c3e0cbfeae2238828323d86d9
King <kingeshiebor@gmail.com>
2024-06-08 14:54:40 +0100

    Changed router push URL to billboards page

--- app/(dashboard)/[storeid]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeid]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -78,7 +78,7 @@ const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => {
       setLoading(true);
       await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
       router.refresh();
-      router.push("/");
+      router.push(`/${params.storeId}/billboards`);
       toast.success("Billboard Deleted");
     } catch (error) {
       toast.error("Make sure you removed all categories using this billboard first");
