53450280c142a22e8e3412bdf25e4a4c0735fb57
King <kingeshiebor@gmail.com>
2024-06-08 22:34:41 +0100

    Added Categories route UI

---
+++ app/(dashboard)/[storeid]/(routes)/categories/components/columns.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from "./cell-action";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type CategoryColumn = {
+  id: string;
+  name: string;
+  billboardLabel: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export const columns: ColumnDef<CategoryColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "billboard",
+    header: "Billboard",
+    cell: ({row}) => row.original.billboardLabel,
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date Created",
+  },
+  {
+    accessorKey: "updatedAt",
+    header: "Last Modified",
+  },
+  {
+    id: "actions",
+    cell: ({row}) => <CellAction data={row.original} />,
+  },
+];
