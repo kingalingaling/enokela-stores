756db1de9e1eefffa58ef43ba761c7d6d2106798
kingalingaling <kingeshiebor@gmail.com>
2025-05-29 14:47:07 +0100

    Added fix for undefined storeId in URL

--- app/(dashboard)/[storeid]/layout.tsx
+++ app/(dashboard)/[storeid]/layout.tsx
@@ -29,7 +29,7 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <Navbar />
+      <Navbar storeId={params.storeId} />
       {children}
     </>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,7 @@ import StoreSwitcher from "./store-switcher";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
 
-const Navbar = async () => {
+const Navbar = async ({storeId}:{storeId:string}) => {
   const { userId } = auth();
 
   if (!userId) {
@@ -23,7 +23,7 @@ const Navbar = async () => {
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <StoreSwitcher items={stores} />
-        <MainNav className="mx-6" />
+        <MainNav className="mx-6" storeId={storeId} />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
         </div>
